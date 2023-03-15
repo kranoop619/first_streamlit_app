975bd6b918b6ba1a828b0546952775630705aaca
kranoop619 <51877518+kranoop619@users.noreply.github.com>
2023-03-15 23:09:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 
 import streamlit
 streamlit.title("New healty Dinner")
-streamlit.header("Menu")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Omlet")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-boiled Free-range egg")
