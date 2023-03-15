907bb149118a5eae5fda77aec2fba1a6f1d235bc
kranoop619 <51877518+kranoop619@users.noreply.github.com>
2023-03-15 23:14:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ streamlit.header("Breakfast Menu")
 streamlit.text("🥣 Omega 3 & Blueberry Omlet")
 streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-boiled Free-range egg")
+streamlit.text("🥑🍞 Avacado Toast")
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
